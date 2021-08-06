bf5d1dfead17164e209a69da31dc1715aa6aaccc
hmtmcse <hmtm.cse@gmail.com>
2021-08-06 18:50:34 +0600

    bismillah

---
+++ src/main/java/com/problemfighter/texttoweb/TextToWebApplication.java
@@ -0,0 +1,13 @@
+package com.problemfighter.texttoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextToWebApplication.class, args);
+	}
+
+}
